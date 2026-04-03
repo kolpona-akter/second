75df52d9eafe66577ab5badd385f39867e55ba21
Mst Surnaly Akter <surnalyms@gmail.com>
2026-04-03 13:20:36 +0600

    hello

--- index.js
+++ index.js
@@ -3,4 +3,15 @@ console.log()
 /**
  * git init
  * git add .
+ * git commit -m "added navbar"
+ * git branch -M main
+ * git remote add origin https://github.com/kolpona-akter/second.git
+ * git remote set-url origin git@github.com:kolpona-akter/second.git
+ *  git push --set-upstream origin main
+ */
+
+
+/**
+ * git add .
+ * git commit -m "hello"
  */
